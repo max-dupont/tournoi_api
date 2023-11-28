4ecd8249de1da51252a023489cff5fecc50d1750
Maxime Dupont <maxime.dupont49280@gmail.com>
2023-11-28 16:33:00 +0100

    feat(rooms): update games class

--- classes/games-class.js
+++ classes/games-class.js
@@ -10,14 +10,16 @@ let Games = class {
     static getAll() {
         return new Promise((next) => {
             db.query(`
-                    SELECT g.id as id, tower, number,
+                    SELECT g.id as id, tower, g.number as number,
                     first_player, CONCAT(p1.firstname,' ', p1.lastname) as first_player_name,
                     second_player, CONCAT(p2.firstname,' ', p2.lastname) as second_player_name,
-                    winner, CONCAT(w.firstname,' ', w.lastname) as winner_name
+                    winner, CONCAT(w.firstname,' ', w.lastname) as winner_name,
+                    r.number as room_number
                     FROM games g 
                     LEFT JOIN players p1 ON g.first_player=p1.id
                     LEFT JOIN players p2 ON g.second_player=p2.id
                     LEFT JOIN players w ON g.winner=w.id
+                    LEFT JOIN rooms r ON g.room=r.id
                 `)
                 .then(result => next(result))
                 .catch(err => next(err))
@@ -25,7 +27,7 @@ let Games = class {
     }
     static addOne(game) {
         return new Promise((next) => {
-            db.query('INSERT INTO games(tower, number, first_player, second_player) VALUES(?, ?, ?, ?)', [game.tower, game.number, game.first_player, game.second_player])
+            db.query('INSERT INTO games(tower, number, first_player, second_player, room) VALUES(?, ?, ?, ?, ?)', [game.tower, game.number, game.first_player, game.second_player, game.room])
                 .then(result => next(result))
                 .catch(err => next(err))
         })
@@ -40,6 +42,10 @@ let Games = class {
         if (game.second_player) {
             req += 'second_player = ?, '
             body.push(game.second_player)
+        }
+        if (game.room) {
+            req += 'room = ?, '
+            body.push(game.room)
         }
         req += 'winner = ? WHERE tower = ? AND number = ?'
         body.push(game.winner, game.tower, game.number)
